7bf7d450b253a68526a9da5b55331eb05326f79b
TomaGuci <thomas.guci@gmail.com>
2023-03-29 15:07:27 +0200

    Allied Grid

--- src/main/java/GUI_Frontend/AlliedGrid.java
+++ src/main/java/GUI_Frontend/AlliedGrid.java
@@ -2,21 +2,22 @@ package GUI_Frontend;
 
 import javax.swing.JPanel;
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridBagLayout;
+import java.awt.Toolkit;
 
 
 
 public class AlliedGrid extends JPanel {
 
-//    public AlliedGrid() {
-//        OceanGR ocean = new OceanGR();
-//
-//        JPanel container = new JPanel(new BorderLayout());
-//        container.add(ocean, BorderLayout.CENTER);
-//
-//        BoardGUI board = new BoardGUI();
-//        board.setDefaultCloseOperation(BoardGUI.EXIT_ON_CLOSE);
-//        board.getContentPane().add(container);
-//        board.pack();
-//        board.setVisible(true);
-//    }
+    public AlliedGrid() {
+
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        //this.setLayout(new BorderLayout());
+        this.setBounds(screenSize.width/3,0, screenSize.width/3, screenSize.height);
+        this.setBackground(Color.RED);
+
+
+    }
 }
