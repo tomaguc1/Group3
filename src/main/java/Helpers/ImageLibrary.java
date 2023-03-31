e3743caabe2bdecf397f33798fcdc8d439b7df41
TomaGuci <thomas.guci@gmail.com>
2023-03-31 13:52:44 +0200

    Changes to UI gifs added

--- src/main/java/Helpers/ImageLibrary.java
+++ src/main/java/Helpers/ImageLibrary.java
@@ -3,5 +3,6 @@ package Helpers;
 import javax.swing.*;
 
 public class ImageLibrary {
-    public ImageIcon shipImage = new ImageIcon(getClass().getResource("/Miscellaneous/battleshipAppLogo.png"));
+    public ImageIcon shipImage = new ImageIcon(getClass().getResource("/Miscellaneous/russShip.gif"));
+    public ImageIcon amogusImage = new ImageIcon(getClass().getResource("/Miscellaneous/amogus.png"));
 }
--- src/main/java/Views/MenuConnect.java
+++ src/main/java/Views/MenuConnect.java
@@ -4,36 +4,48 @@ import Helpers.ImageLibrary;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class MenuConnect extends JFrame {
+public class MenuConnect extends JFrame implements ActionListener {
 
     ImageLibrary graphics = new ImageLibrary();
     public MenuConnect(){
         //Frame settings
         this.setTitle("MenuConnect Title !");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setResizable(true);
-        this.setSize(640,640);
+        this.setResizable(false);
+        this.setSize(1280,720);
         this.setVisible(true);
         this.getContentPane().setBackground(new Color(250,250,250));
 
         //Label Picture + Text
-        JLabel label = new JLabel();
-        label.setText("Battleships Group3");
         ImageIcon image = new ImageIcon(graphics.shipImage.getImage());
-        label.setIcon(image);
+        JLabel label = new JLabel(image);
+        label.setText("Battleships Group3");
         label.setVerticalAlignment(JLabel.TOP);
         label.setHorizontalAlignment(JLabel.CENTER);
-            //text allignment
         label.setHorizontalTextPosition(JLabel.CENTER);
         label.setVerticalTextPosition(JLabel.BOTTOM);
         label.setForeground(new Color(105,0,0));
         label.setFont(new Font("Helvetica", Font.BOLD, 28));
 
+        //Pannels
+        JPanel greenPanel = new JPanel();
+        greenPanel.setBackground(Color.GREEN);
+        greenPanel.setBounds(0,0,64,64);
+            JLabel amogusLabel = new JLabel(new ImageIcon(graphics.amogusImage.getImage()));
+        greenPanel.add(amogusLabel);
 
-        this.add(label);        //Adding label to the frame
+            //ADD components to the frame
+        this.add(greenPanel);
+        this.add(label);        //this Frame had label added to it
 
 
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
 
     }
 }
