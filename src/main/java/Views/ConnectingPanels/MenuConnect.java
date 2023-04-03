751fbc322d9533b4c07578ad69bd30c24860bccd
TomaGuci <thomas.guci@gmail.com>
2023-04-04 00:57:32 +0200

    snapshot 2

--- src/main/java/Views/ConnectingPanels/MenuConnect.java
+++ src/main/java/Views/ConnectingPanels/MenuConnect.java
@@ -20,34 +20,17 @@ public class MenuConnect extends JFrame{
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setResizable(false);
         this.setSize(1280,720);
-        this.setVisible(true);
-        this.getContentPane().setBackground(new Color(68, 159, 175));
+        this.getContentPane().setBackground(Color.MAGENTA);
         this.setLayout(new BorderLayout(10,15));        //----------------------------------------------------------------------------------------------------------------------
 //======================================================================================================================
 
-        //Label Picture + Text
-        ImageIcon image = new ImageIcon(graphics.shipImage.getImage());
-        JLabel menuLabel_shipLabel = new JLabel(image);
-        menuLabel_shipLabel.setText("Battleships Group3");
-        menuLabel_shipLabel.setVerticalAlignment(JLabel.TOP);
-        menuLabel_shipLabel.setHorizontalAlignment(JLabel.CENTER);
-        menuLabel_shipLabel.setHorizontalTextPosition(JLabel.CENTER);
-        menuLabel_shipLabel.setVerticalTextPosition(JLabel.BOTTOM);
-        menuLabel_shipLabel.setForeground(new Color(105,0,0));
-        menuLabel_shipLabel.setFont(new Font("Helvetica", Font.BOLD, 28));
 
         //Pannels
         servingDecisionPANEL = new ServDecisionPanel();
-
-
-
-
-
-
-        //ADD components to the frame
+//ADD components to the frame
         //''this'' -- Frame Component
-        this.add(menuLabel_shipLabel);
-        this.add(servingDecisionPANEL, BorderLayout.SOUTH);
+        this.add(servingDecisionPANEL);
+        this.setVisible(true);
 
 
     }//End of constrctor
--- src/main/java/Views/ConnectingPanels/ServDecisionPanel.java
+++ src/main/java/Views/ConnectingPanels/ServDecisionPanel.java
@@ -1,5 +1,7 @@
 package Views.ConnectingPanels;
 
+import Helpers.ImageLibrary;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -12,37 +14,75 @@ enum SERVICE{
 
 
 public class ServDecisionPanel extends JLayeredPane implements ActionListener {
+    ImageLibrary graphics = new ImageLibrary();
     JButton buttonHost;
     JButton buttonClient;
     SERVICE service;
-
+    ImageIcon image = new ImageIcon(graphics.shipImage.getImage());
+    JLabel menuLabel_shipLabel;
     JPanel basePanel;
     JPanel clientConPanel;
     JPanel hostConPanel;
     public ServDecisionPanel() {
+
         this.setDoubleBuffered(true);
         this.setPreferredSize(new Dimension(100, 200));
-        this.setLayout(new FlowLayout());
+        this.setLayout(new BorderLayout());
+//Label Ship + Text
+        menuLabel_shipLabel = new JLabel(image);
+        menuLabel_shipLabel.setSize(new Dimension(500,400));
+        menuLabel_shipLabel.setText("Battleships Group3");
+        menuLabel_shipLabel.setVerticalAlignment(JLabel.TOP);
+        menuLabel_shipLabel.setHorizontalAlignment(JLabel.CENTER);
+        menuLabel_shipLabel.setHorizontalTextPosition(JLabel.CENTER);
+        menuLabel_shipLabel.setVerticalTextPosition(JLabel.BOTTOM);
+        menuLabel_shipLabel.setForeground(new Color(105,0,0));
+        menuLabel_shipLabel.setFont(new Font("Helvetica", Font.BOLD, 28));
+            menuLabel_shipLabel.setOpaque(true);
+            menuLabel_shipLabel.setBackground(Color.GREEN);
+        JPanel menuLabel_Pane = new JPanel();
+        menuLabel_Pane.add(menuLabel_shipLabel);
+
+
+
+
         basePanel = new JPanel();
+        basePanel.setSize(1280,720);
+            basePanel.setLayout(new BorderLayout());
+            basePanel.setBackground(Color.RED); //TODO: REMOVE
+        //BasePanel initialization
+        basePanel.add(menuLabel_shipLabel);
+//        basePanel.add(buttonHost);
+//        basePanel.add(buttonClient);
 
 
-        //====  Connection menu Pannel ==== TODO :{ after pressing host should display the ip address to connect at, and handle a ready lock to transition to the game }
-
-        //Button for Hosting
-//HOST  :
+        hostConPanel = new Connecting_hostPanel();
+        clientConPanel = new Connecting_clientPanel();
+//Button for Hosting
+        //HOST  :
         buttonHost = new ButtonClient();
+        buttonHost.addActionListener(e -> {
+            System.out.println("->hostButton pressed !");
+            basePanel.setVisible(false);
+            this.add(hostConPanel, Integer.valueOf(1));
+        });
 
-//CLIENT:buttonClient = new ButtonClient();
+        buttonClient = new ButtonClient();
+        buttonClient.addActionListener(e -> {
+            System.out.println("->clientButton pressed !");
+            basePanel.setVisible(false);
+            this.add(clientConPanel, Integer.valueOf(1));
+        });
+
 
 //----------------------------------------------------------------------------------------------------------------------
 //======================================================================================================================
 
 
-        //@button_Subpanel
 
-        basePanel.add(buttonHost);
-        basePanel.add(buttonClient);
-        this.add(basePanel, Integer.valueOf(0));
+        this.add(menuLabel_Pane, BorderLayout.CENTER);
+
+        basePanel.setVisible(true);
         this.setVisible(true);
 
         //ADD components to the frame
