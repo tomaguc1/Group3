8e42150615e607362e40a6972b0d5ca9ea0d36a0
bruger <bruger@10.209.113.221>
2023-04-12 15:01:37 +0200

    new ships

--- src/main/java/Model/Board/LogicShipRun.java
+++ src/main/java/Model/Board/LogicShipRun.java
@@ -7,25 +7,66 @@ public class LogicShipRun {
 		b.printBoard();
 		
 		Scanner s = new Scanner(System.in);
+		
+		System.out.println("Enter ship: ");
+		String shipType = s.nextLine();
+		
 		System.out.println("Enter X:");
 		int x=s.nextInt();
 		System.out.println("Enter Y:");
 		int y=s.nextInt();
 		
 		
+		
+		/*
 		Ship titanic = new Ship();
 		titanic.setLength(5);
 		titanic.setPosition(y, x);
 		titanic.setDirection(Direction.HORIZONTAL);
 		b.setShip(titanic.position, titanic);
 		b.printBoard();
+		*/
 		
+		if (shipType.equals("carrier")) {
+			Ship Carrier = new Ship("Carrier");
+			Carrier.setPosition(x, y);
+			b.setShip(Carrier.position, Carrier);
+			b.printBoard();
+		}
+		else if (shipType.equals("destroyer")) {
+			Ship Destroyer = new Ship("Destroyer");
+			Destroyer.setPosition(x, y);
+			b.setShip(Destroyer.position, Destroyer);
+			b.printBoard();
+		}
+		else if (shipType.equals("battleship")) {
+			Ship BattleShip = new Ship("BattleShip");
+			BattleShip.setPosition(x, y);
+			b.setShip(BattleShip.position, BattleShip);
+			b.printBoard();
+		}
+		else if (shipType.equals("submarine")) {
+			Ship Submarine = new Ship("Submarine");
+			Submarine.setPosition(x, y);
+			b.setShip(Submarine.position, Submarine);
+			b.printBoard();
+		}
+		else if (shipType.equals("patrol")) {
+			Ship Patrol = new Ship("Patrol");
+			Patrol.setPosition(x, y);
+			b.setShip(Patrol.position, Patrol);
+			b.printBoard();
+		}
+		else {
+			System.out.println("Bye bye loser");
+		}
 		
-	
-		
-		
-		
-		
+		/*
+		Ship D = new Ship("Destroyer");
+		D.setPosition(x, y);
+		b.setShip(D.position, D);
+		b.printBoard();
+		*/
 		/*
 		Scanner s = new Scanner(System.in);
 		System.out.println("Enter X:");
--- src/main/java/Model/Board/Ship.java
+++ src/main/java/Model/Board/Ship.java
@@ -1,10 +1,46 @@
 package Model.Board;
+
+
 public class Ship {
 	private Direction direction;
 	private int length;
 	protected Position position;
 	protected int x;
 	protected int y;
+	
+	public Ship() {
+		
+	}
+	
+	public Ship (String shipType) {
+		switch (shipType) {
+		case "Carrier": 
+			this.setLength(5);
+			setDirection(Direction.VERTICAL);
+		break;
+		
+		case "BattleShip":
+			this.setLength(4);
+			setDirection(Direction.VERTICAL);
+		break;
+		
+		case "Destroyer":
+			this.setLength(3);
+			setDirection(Direction.HORIZONTAL);
+		break;
+		
+		case "Submarine":
+			this.setLength(2);
+			setDirection(Direction.HORIZONTAL);
+		break;
+		
+		case "Patrol":
+			this.setLength(1);
+			setDirection(Direction.HORIZONTAL);
+		break;
+		
+		}
+	}
 	
 	public void setLength (int length) {
 		this.length=length;
