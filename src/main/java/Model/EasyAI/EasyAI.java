f2e8ca77ac0a8f7f8cd2dbb72cc0c90581d07c87
Ramm23 <53792824+Ramm23@users.noreply.github.com>
2023-04-20 17:15:28 +0200

    Easy AI
    Added a way for it to set both ships and missile

--- src/main/java/Model/EasyAI/EasyAI.java
+++ src/main/java/Model/EasyAI/EasyAI.java
@@ -1,33 +1,69 @@
 package Model.EasyAI;
 
 import java.util.Random;
-
 import java.util.ArrayList;
-import java.util.List;
 
 public class EasyAI {
     public static void main(String[] args) {
         
     }
-    
-    public void test(){
-    	List<String> coordinates = new ArrayList<>();
+
+    public void setShips(){
+        ArrayList<String> coordinates = new ArrayList<String>();
         for (int x = 1; x <= 10; x++) {
             for (int y = 1; y <= 10; y++) {
                 coordinates.add("(" + x + "," + y + ")");
-                }
             }
-           
-    	Random r = new Random();
-    	int i = 100;
-    	while(i > 95) {
-    		int testing = r.nextInt(i);
-    		coordinates = coordinates.remove(testing);
-    		i = i - 1;
-    		
+        }
+        Random rand = new Random();
+        int min = 0;
+        int max = 99;
+        int i=0;
+
+    	while(i <= 5) {
+            int testing = rand.nextInt(max - min + 1) + min;//random number for a coordinate in the array
+
+            if(coordinates.get(testing) != null) {
+                //method to set ship
+                coordinates.set(testing, null);// Removes the space from the array, keeps array length
+            }else{
+                i=i-1;
+                }
+            i++;
+            }
     	}
     	
-    	
+
+
+    public void setMissile(){
+        ArrayList<String> coordinates = new ArrayList<String>();
+        for (int x = 1; x <= 10; x++) {
+            for (int y = 1; y <= 10; y++) {
+                coordinates.add("(" + x + "," + y + ")");
+            }
+        }
+        Random rand = new Random();
+
+
+        int i=0;
+        while(i < 100 ) {
+
+            int min = 0;
+            int max = 99-i;
+
+            int rCoordinate = rand.nextInt(max - min + 1) + min;//random number for a coordinate in the array
+
+            if(coordinates.get(rCoordinate) != null){//an array containing all the coordinates in which enemy ships are placed) {
+                //method to set missile
+                coordinates.remove(rCoordinate);// Removes the space from the array, reduces array length
+                //end turn
+            }else{
+            //method to set missile
+            coordinates.remove(rCoordinate);// Removes the space from the array, reduces array length
+            //continue turn
+
+            }i++;
     }
-  
-}
+
+
+}}
