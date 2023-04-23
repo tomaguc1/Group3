433f7e03720b010b078e30464bc86da7d60ebc47
TomaGuci <thomas.guci@gmail.com>
2023-04-23 14:24:53 +0200

    s

--- src/main/java/Views/PlaceShips/GridShip/BoardandIndex_Layered.java
+++ src/main/java/Views/PlaceShips/GridShip/BoardandIndex_Layered.java
@@ -6,10 +6,12 @@ import java.awt.*;
 public class BoardandIndex_Layered extends JLayeredPane {
 
     public BoardandIndex_Layered(){
+
         init_GridPane();
     }
     void init_GridPane(){
         setLayout(null);
         setPreferredSize(new Dimension(550, 550));
         setBackground(Color.MAGENTA);
+    }
 }
