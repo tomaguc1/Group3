cfb1c4dc0a44668281514b4b44861629ff19815b
Viraj <s215221@dtu.dk>
2023-04-23 18:19:51 +0200

    Cucumber for VolcanicIsland

---
+++ src/test/java/StepsDefObstacles.java
@@ -0,0 +1,21 @@
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class StepsDefObstacles {
+
+	
+	@Given("I have a board")
+	public void i_have_a_board() {
+	    
+	}
+	@When("I place a volcanic island")
+	public void i_place_a_volcanic_island() {
+	  
+	}
+	@Then("a volcanic island is placed")
+	public void a_volcanic_island_is_placed() {
+	    
+	}
+
+}
---
+++ src/test/resources/OurUserFiles/Obstacles.feature
@@ -0,0 +1,10 @@
+
+@tag
+Feature: Placement of obstacles
+
+  @tag1
+  Scenario: Placing Volcanic island
+    Given I have a board
+    When I place a volcanic island
+    Then a volcanic island is placed
+ 
