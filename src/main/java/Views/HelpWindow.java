e3efcb67ede0bf13bb35be7c553501faf666c61b
s195687@gitlab.gbar.dtu.dk <s195687@student.dtu.dk>
2023-04-30 20:41:47 +0200

    Changed Layout Help

--- src/main/java/Views/HelpWindow.java
+++ src/main/java/Views/HelpWindow.java
@@ -10,7 +10,7 @@ public class HelpWindow {
             "Above the grid, you can see a box where you can type in your name.\n" +
             "After choosing your desired game mode, place your ship in an unpredictable manner. You can place your ships by clicking on the ship icon on the right side of the grid. From there, hover over the grid. You should see the shape of the ship you just selected. Use the right click button on your mouse to rotate the ship. You can now place your ship.\n" +
             "Repeat those steps until you have no ship left.\n" +
-            "\n" +
+            
             "You can now press the “Lock In” button next to your name to proceed to the game.\n" +
             "\n" +
             "On the left side of your screen, you should be able to see your grid where you placed your ships. This is where the opponent’s shot will be shown.\n" +
@@ -26,11 +26,11 @@ public class HelpWindow {
     JButton jButton = new JButton();
 
         public HelpWindow(){
-            jLabel.setBounds(0,0,1280,720);
+            jLabel.setPreferredSize(new Dimension(1200,700));
             jLabel.setHorizontalAlignment(JLabel.CENTER);
             jLabel.setVerticalAlignment(JLabel.TOP);
 
-            jFrame.setLayout(null);
+            jFrame.setLayout(new FlowLayout());
             jFrame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
             jFrame.setSize(1280,720);
             jFrame.add(jLabel);
