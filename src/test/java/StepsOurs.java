841b03437da7e6f555fabcf1d9efb6cd4b902b65
bruger <bruger@10.209.113.221>
2023-04-30 11:28:54 +0200

    the stories that work without changing the model

--- src/test/java/StepsOurs.java
+++ src/test/java/StepsOurs.java
@@ -21,93 +21,10 @@ public class StepsOurs {
 	
 	Board b = new Board();
 	Ship s;
-	Position position;
-	ArrayList<Ship> shipArray;
-	int shipLength;
 	
 	
-	@Given("Player {int} with some ships")
-	public void player_with_some_ships(Integer int1)  {
-		shipArray = new ArrayList<Ship>();
-	    PlayerModel player = new PlayerModel("Player" + int1, shipArray);
-		for (Ship_Type type : Ship_Type.values()) {
-			shipArray.add(new Ship(type));
-		}
-	}
-	@When("{int} chooses to place a {string} of length")
-	public void chooses_to_place_a_of_length(Integer int1, String string) {
-		for (Ship ship : shipArray) {
-			if (ship.getShipType().toString().equals(string.toUpperCase())) {
-				s = ship;
-				shipLength = ship.getShipType().length();
-				
-			} else {
-				continue;
-			}
-		}                
-	}
-
-	@Then("Has the appropiate length")
-	public void has_the_appropiate_length() {
-		
-		assertEquals(s.getLength(),shipLength);
-	}
-	
-	@Given("Player {int} with some ships")
-	public void player_with_ships_to_place(Integer int1) {
-		shipArray = new ArrayList<Ship>();
-	    PlayerModel player = new PlayerModel("Player" + int1, shipArray);
-		for (Ship_Type type : Ship_Type.values()) {
-			shipArray.add(new Ship(type));
-		}
-	}
-	@When("places on the position {int} {int}")
-	public void places_on_the_position(Integer int1, Integer int2)  {
-		
-        s = shipArray.get(0);
-        b.setShip( new Position(int1,int2),s);
-        
-        
-	}
-	@Then("there should be a ship on the positon {int} {int}")
-	public void there_should_be_a_ship_on_the_positon(Integer int1, Integer int2) {
-		assertTrue(b.getBoardElementAtPosition(new Position(int1,int2)) instanceof ShipElement);
-	}
-
-	ShipElement shipElement;
-
-	@Given("{string} on the position {int} {int} with health {int}")
-	public void on_the_position_with_health(String string, Integer int1, Integer int2, Integer int3) {
-		//Ship_Type type = Ship_Type.valueOf(string.toUpperCase());
-        //s = new Ship(type);
-        //b.setShip( new Position(int1,int2),s);
-		shipElement = new ShipElement();
-		position = new Position(int1, int2);
-		b.setBoardElementTypeAtPosition(shipElement, position);
-		assertTrue(b.getBoardElementAtPosition(position) instanceof ShipElement);
-		//throw new io.cucumber.java.PendingException();
-	}
-	@When("It was hit by a move on the position {int} {int}")
-	public void it_was_hit_by_a_move_on_the_position(Integer int1, Integer int2) {
-			
-		position = new Position(int1, int2);
-		shipElement = (ShipElement) b.getBoardElementAtPosition(position);
-		//Ship s = (ShipElement) b.getBoardElementAtPosition(new Position(1, 1));
-	    shipElement.setWasHit();
-		assertTrue(shipElement.getWasHit()==true);
-	   
-	}
-	@Then("the health is {int}")
-	public void the_health_is(Integer int3) {
-	   assertEquals(b.getHealth(), int3 -1);
-	}
-
-	
-	
-}
-	/*
-	@Given("Player {int} boar view")
-	public void player_boar_view(Integer int1) {
+	@Given("Player {int} board view")
+	public void player_board_view(Integer int1) {
 	    
 	}
 	@When("{string} chooses to place a {string}")
@@ -116,136 +33,29 @@ public class StepsOurs {
         s = new Ship(type);
         
 	}
-	@Then("{string} has the length {int}")
-	public void has_the_length(String string, int int1) {
+	
+	@Then("{string} has the length {string}")
+	public void has_the_length(String string, String string2) {
+		int x = Integer.parseInt(string2);
+		assertEquals(s.getLength(),x);
+	}
+	@Given("Player {string} board view")
+	public void player_board_view(String string) {
 	    
-		assertEquals(s.getLength(),int1);
-		
 	}
 	
+	@When("{string} places {string}  on the position {int} {int}")
+	public void places_on_the_position(String string, String string2, int int1, int int2) {
+		Ship_Type type = Ship_Type.valueOf(string2.toUpperCase());
+        s = new Ship(type);
+        b.setShip( new Position(int1,int2),s);
+        
+        
+	}
+	@Then("there should be a ship on the initial positon {int} {int}")
+	public void there_should_be_a_ship_on_the_initial_positon(int int1, int int2) {
+ 		assertTrue(b.getBoardElementAtPosition(new Position(int1,int2)) instanceof Ship);
+ 	}
+
 }
-	
-/*	
-	Ship s;
-	Board b = new Board();
-	Bomb bomb= new Bomb();
-	int x;
-	int y;
-	
-	//Player player = new Player;
-	//Miss miss = new Miss();
-	
-	@Given("A {string}")
-	public void a(String string) {
-		s = new Ship (string);
-
-	}
-
-	
-	@When("I am placing the ship on the position {string} {string}")
-	public void i_am_placing_the_ship_on_the_position(String string, String string2) {
-		int x = Integer.parseInt(string);
-		int y = Integer.parseInt(string2);
-		s.setPosition(x,y);	
-		b.setShip(s.getPosition(), s);
-	
-	}
-	
-	
-	@Then("There should be a ship on the initial position {string} {string}")
-	public void there_should_be_a_ship_on_the_initial_position(String string, String string2) {
-		int x = Integer.parseInt(string);
-		int y = Integer.parseInt(string2);
-		assertTrue(b.getBoardElement(new Position(x,y)) instanceof Ship);
-	}
-	@Then("There should be a ship on the final position  {string} {string}")
-	public void there_should_be_a_ship_on_the_final_position(String string, String string2) {
-		int xEnd = Integer.parseInt(string);
-		int yEnd = Integer.parseInt(string2);
-		assertTrue(b.getBoardElement(new Position(xEnd,yEnd)) instanceof Ship);
-	}
-	
-	
-	@Given("Two players: {string} and {string}")
-	public void two_players_and(String string, String string2) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
-	}
-
-	@When("{string} is finished with setting his ships")
-	public void is_finished_with_setting_his_ships(String string) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
-	}
-
-	@Then("{string} can set his ships")
-	public void can_set_his_ships(String string) {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
-	}
-	
-	
-	
-	
-	
-	/*
-	
-	@Given("My board")
-	public void my_board() {
-	    b.printBoard();
-	}
-	@When("I am choosing the position to attack {string} {string}")
-	public void i_am_choosing_the_position_to_attack(String string, String string2) {
-		int x = Integer.parseInt(string);
-		int y = Integer.parseInt(string2);
-		
-		//b.setBomb(x,y,bomb, miss); ?? Is miss necesarry 
-	}
-	@Then("A bomb replaces the ship at that position and you can shoot again")
-	public void a_bomb_replaces_the_ship_at_that_position_and_you_can_shoot_again() {
-	    //how to make it shoot again
-		
-		b.printBoard();
-	}
-	
-	@Given("I am a player")
-	public void i_am_a_player() {
-	    Player player = new Player(); //why player cannnot be resolved to a type?
-		
-	}
-	@Then("I have a board view")
-	public void i_have_a_board_view() {
-	   
-		
-	}
-	
-	
-	@Given("I have a board")
-	public void i_have_a_board() {
-	    b.printBoard();
-
-	}
-	
-	@When("I am choosing the position to attack {string} {string} and there is an obstacle")
-	public void i_am_choosing_the_position_to_attack_and_there_is_an_obstacle(String string, String string2) {
-		int x = Integer.parseInt(string);
-		int y = Integer.parseInt(string2);
-		//b.setBomb();
-		
-	}
-	@Then("the obstacle explodes")
-	public void the_obstacle_explodes() {
-	    
-		
-		
-	}
-
-*/
-	
-
-
-
-
-
-
 
--- src/test/resources/OurUserFiles/UserStoriesShips_new.feature
+++ src/test/resources/OurUserFiles/UserStoriesShips_new.feature
@@ -18,12 +18,11 @@ Feature: List of scenarios.
     #  | Patrol     |          1 |         1 |1            |1             |
       
      
-  @tag1
+ @tag1
   Scenario Outline: Picking a ship
-  Given Player 1 with some ships
-  When 1 chooses to place a "Ship" of length
-  Then Has the appropiate length
-
+  Given Player 1 board view
+  When "1" chooses to place a "<ShipType>"
+  Then "<ShipType>" has the length "<length>"
   
   Examples:
   | ShipType   |  length  |
@@ -32,23 +31,28 @@ Feature: List of scenarios.
   | Destroyer  |     3    |
   | Submarine  |     2    |
   
-  @tag2
-  Scenario Outline: Placing a ship
-  Given Player "1" board view
-  When places on the position 2 2
-  Then there should be a ship on the positon 2 2
- 	
- 	Examples:
- 	| ShipType   |
- 	| Carrier    |
-  | BattleShip |
-  | Destroyer  |
-  | Submarine  |
+ @tag2
+   Scenario: Placing a ship
+   Scenario Outline: Placing a ship
+   Given Player "1" board view
+   When "1" places "carrier"  on the position 2 2
+   When "1" places "<ShipType>"  on the position 2 2
+   Then there should be a ship on the initial positon 2 2
+
+
+  	Examples:
+  	| ShipType   |
+  	| Carrier    |
+   | BattleShip |
+   | Destroyer  |
+   | Submarine  |
+
   
-  @tag3
-  Scenario Outline: Health check
-  Given "ShipElelment" on the position 1 1 with health 1
-  When It was hit by a move on the position 1 1
-  Then the health is 0
+  
+ # @tag3
+ # Scenario Outline: Health check
+  #Given "ShipElelment" on the position 1 1 with health 1
+ # When It was hit by a move on the position 1 1
+  #Then the health is 0
   
  
